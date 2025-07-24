12f39ba3f0a509b8b578e6c3e0512d3382c44fce
Ayush singh <aayushssingh2003@gmail.com>
2025-07-24 23:59:07 +0530

    update order  model

---
+++ models/Order.models.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";    
+
+const orderschema = mongoose.schema({
+    orderId : {
+        type : Number,
+        required : true ,
+        unique : true 
+    },
+    placedby : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true,
+    },
+    address : {
+        type : String,
+        required : true,
+    },
+    paymentStatus : {
+        type : String,
+        enum : ["Pending", "Completed", "Failed"],
+        default : "Pending",
+
+    },
+    phoneNo : {
+        type : Number,
+        required : true,
+        unique : true,
+    },
+    orderstatus : {
+        type : String,
+        enum : ["Pending", "Shipped", "Delivered", "Cancelled"],
+        default : "Pending",
+    },
+
+
+
+} , {});
+
+
+
+
+
+export const order = mongoose.model("order",orderschema);
